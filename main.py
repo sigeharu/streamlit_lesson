4b1461e19f55e39c770abd315953d19c9b1e8c4b
sigeharu_mtmo <62659450+sigeharu@users.noreply.github.com>
2022-08-04 23:34:25 +0900

    2st commit

--- main.py
+++ main.py
@@ -1,33 +1,85 @@
-import time
-
 import pandas as pd
+import yfinance as yf
+import altair as alt
 import streamlit as st
-import numpy as np
-from PIL import Image
 
-st.title('Streamlit 超入門')
+st.title('米国株価可視化アプリ')
 
-st.write('プレグレスバーの表示')
-'Start!!'
+st.sidebar.write("""
+# GAFA株価
+こちらは株価可視化ツールです。以下のオプションから表示日数を指定できます。
+""")
 
-latest_iteration = st.empty()
-bar = st.progress(0)
+st.sidebar.write("""
+## 表示日数選択
+""")
 
-for i in range(100):
-    latest_iteration.text(f'Iteration {i + 1}')
-    bar.progress(i + 1)
-    time.sleep(0.1)
+days = st.sidebar.slider('日数', 1, 50, 20)
 
-'Done!!!'
+st.write(f"""
+### 過去 **{days}日間** のGAFA株価
+""")
 
-left_column, right_column = st.columns(2)
-button = left_column.button('右カラムに文字を表示')
-if button:
-    right_column.write('ここは右カラム')
 
-expander1 = st.expander('問い合わせ1')
-expander1.write('お問い合わせ1の回答')
-expander1 = st.expander('問い合わせ2')
-expander1.write('お問い合わせ2の回答')
-expander1 = st.expander('問い合わせ3')
-expander1.write('お問い合わせ3の回答')
+@st.cache
+def get_data(days, tickers):
+    df = pd.DataFrame()
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')
+        hist.index = hist.index.strftime('%d %B %Y')
+        hist = hist[['Close']]
+        hist.columns = [company]
+        hist = hist.T
+        hist.index.name = 'Name'
+        df = pd.concat([df, hist])
+    return df
+
+
+try:
+    st.sidebar.write("""
+    ## 株価の範囲指定
+    """)
+    ymin, ymax = st.sidebar.slider(
+        '範囲を指定してください。',
+        0.0, 3500.0, (0.0, 3500.0)
+    )
+
+    tickers = {
+        'apple': 'AAPL',
+        'facebook': 'FB',
+        'google': 'GOOGL',
+        'microsoft': 'MSFT',
+        'netflix': 'NFLX',
+        'amazon': 'AMZN'
+    }
+    df = get_data(days, tickers)
+    companies = st.multiselect(
+        '会社名を選択してください。',
+        list(df.index),
+        ['google', 'amazon', 'facebook', 'apple']
+    )
+
+    if not companies:
+        st.error('少なくとも一社は選んでください。')
+    else:
+        data = df.loc[companies]
+        st.write("### 株価 (USD)", data.sort_index())
+        data = data.T.reset_index()
+        data = pd.melt(data, id_vars=['Date']).rename(
+            columns={'value': 'Stock Prices(USD)'}
+        )
+        chart = (
+            alt.Chart(data)
+            .mark_line(opacity=0.8, clip=True)
+            .encode(
+                x="Date:T",
+                y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+                color='Name:N'
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+except:
+    st.error(
+        "おっと！なにかエラーが起きているようです。"
+    )
